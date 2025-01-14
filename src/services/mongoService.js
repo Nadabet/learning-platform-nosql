cc246d600401caff299e83fec3337ca505e0c7f0
nadabettach <nadabettach6@wgmail.com>
2025-01-14 12:04:54 +0100

    Ajout du modèle Mongoose et des services pour gérer les cours.

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,32 @@
 // Question: Pourquoi créer des services séparés ?
-// Réponse: 
+// Réponse: Cela permet de centraliser les interactions avec la base de données, de rendre le code plus modulaire et de faciliter les tests unitaires.
 
 const { ObjectId } = require('mongodb');
+const db = require('../config/db');
 
-// Fonctions utilitaires pour MongoDB
-async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+async function findOneById(collectionName, id) {
+  const dbInstance = db.getDb();
+  return await dbInstance.collection(collectionName).findOne({ _id: new ObjectId(id) });
 }
 
-// Export des services
+async function insertOne(collectionName, data) {
+  const dbInstance = db.getDb();
+  return await dbInstance.collection(collectionName).insertOne(data);
+}
+
+async function listCourses(collectionName) {
+  const dbInstance = db.getDb();
+  return await dbInstance.collection(collectionName).find().toArray();
+}
+
+async function createCourse(collectionName, courseData) {
+  const dbInstance = db.getDb();
+  return await dbInstance.collection(collectionName).insertOne(courseData);
+}
+
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  insertOne,
+  listCourses,
+  createCourse, 
 };
